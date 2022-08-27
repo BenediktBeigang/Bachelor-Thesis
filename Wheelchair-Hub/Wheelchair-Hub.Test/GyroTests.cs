6b8f17ffdc233c864a2ca0a1508a0632a5ec4537
BenediktBeigang <benedikt.beigang@outlook.de>
2022-08-27 08:48:23 +0200

    27_8__03
    added unitTest for smoothing GyroData

---
+++ Wheelchair-Hub/Wheelchair-Hub.Test/GyroTests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+namespace Wheelchair_Hub.Test;
+
+public class GyroTests
+{
+    private Gyro _Gyro;
+    [SetUp]
+    public void Setup()
+    {
+        _Gyro = new Gyro(GyroMode.GYRO_1000, DeviceNumber.ONE);
+    }
+
+    [Test]
+    public void SmoothValue_ValidValues_CorrectReturnValue()
+    {
+        double[] unfilteredValues = new double[] { 7, 6, 5, 4, 3, 2, 1 };
+
+        double result = _Gyro.SmoothValue(unfilteredValues, 0);
+
+        Assert.That(result, Is.AtLeast(4.54));
+        Assert.That(result, Is.AtMost(4.56));
+        // Assert.That(result.Item1, Is.EqualTo(100));
+        // Assert.That(result.Item2, Is.EqualTo(0));
+        Assert.That(true);
+    }
+
+    [Test]
+    public void SmoothedDegreePerSecondLast_Test()
+    {
+        double[] unfilteredValues = new double[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        double[] result = _Gyro.SmoothedDegreePerSecond_Last(2, unfilteredValues);
+
+        Assert.That(result[0], Is.AtLeast(3.44));
+        Assert.That(result[0], Is.AtMost(3.46));
+        Assert.That(result[1], Is.AtLeast(4.44));
+        Assert.That(result[1], Is.AtMost(4.46));
+    }
+}
--- Wheelchair-Hub/Wheelchair-Hub.Test/RealisticWheelchairTests.cs
+++ Wheelchair-Hub/Wheelchair-Hub.Test/RealisticWheelchairTests.cs
@@ -1,141 +1,134 @@
-using NUnit.Framework;
+// using NUnit.Framework;
 
-namespace Wheelchair_Hub.Test;
+// namespace Wheelchair_Hub.Test;
 
-public class RealisticWheelchairTests
-{
-    private RealisticWheelchair? Transformation;
-    [SetUp]
-    public void Setup()
-    {
-        Transformation = new RealisticWheelchair(30, 55.5);
-    }
+// public class RealisticWheelchairTests
+// {
+//     private RealisticWheelchair? Transformation;
+//     [SetUp]
+//     public void Setup()
+//     {
+//         Transformation = new RealisticWheelchair(30, 55.5, 100, 25);
+//     }
 
-    #region Case 1 - DualWheel_SameDirection
-    [Test]
-    public void NextTransformedValues_Forward_CorrectForward()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = 100;
-        double right = 100;
+//     #region Case 1 - DualWheel_SameDirection
+//     [Test]
+//     public void ValuesNext_Forward_CorrectForward()
+//     {
+//         Rotations rot = new Rotations(0, 0, 100, 100);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//         ControllerInput result = Transformation!.Values_Next(rot);
 
-        Assert.That(result.Item1, Is.EqualTo(100));
-        Assert.That(result.Item2, Is.EqualTo(0));
-    }
+//         Assert.That(result.LeftThumbY, Is.EqualTo(3276));
+//         Assert.That(result.RightThumbX, Is.EqualTo(0));
+//     }
 
-    [Test]
-    public void NextTransformedValues_Backwards_CorrectBackwards()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = -100;
-        double right = -100;
+//     [Test]
+//     public void ValuesNext_Backwards_CorrectBackwards()
+//     {
+//         Rotations rot = new Rotations(, 1000, -30.51, -30.51);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//         ControllerInput result = Transformation!.Values_Next(rot);
 
-        Assert.That(result.Item1, Is.EqualTo(-100));
-        Assert.That(result.Item2, Is.EqualTo(0));
-    }
-    #endregion
+//         Assert.That(result.LeftThumbY, Is.EqualTo(-3276));
+//         Assert.That(result.RightThumbX, Is.EqualTo(0));
+//     }
+//     #endregion
 
-    #region Case 2 - DualWheel_DirectionsAgainstEachOther
-    [Test]
-    public void NextTransformedValues_LeftTurn_CorrectLeftRotation()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = -100;
-        double right = 100;
+//     #region Case 2 - DualWheel_DirectionsAgainstEachOther
+//     [Test]
+//     public void ValuesNext_LeftTurn_CorrectLeftRotation()
+//     {
+//         Rotations rot = new Rotations(0, 0, -100, 100);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//         ControllerInput result = Transformation!.Values_Next(rot);
 
-        Assert.That(result.Item1, Is.EqualTo(0));
-        Assert.That(result.Item2, Is.AtLeast(-207));
-        Assert.That(result.Item2, Is.AtMost(-206));
-    }
+//         Assert.That(result.RightThumbX, Is.EqualTo(0));
 
-    [Test]
-    public void NextTransformedValues_RightTurn_CorrectRightRotation()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = 100;
-        double right = -100;
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//         Assert.That(result.LeftThumbY, Is.EqualTo(0));
+//         Assert.That(result.RightThumbX, Is.EqualTo(-3276));
+//     }
 
-        Assert.That(result.Item1, Is.EqualTo(0));
-        Assert.That(result.Item2, Is.AtLeast(206));
-        Assert.That(result.Item2, Is.AtMost(207));
-    }
-    #endregion
+//     // [Test]
+//     // public void NextTransformedValues_RightTurn_CorrectRightRotation()
+//     // {
+//     //     short rawleft = 0;
+//     //     short rawright = 0;
+//     //     double left = 100;
+//     //     double right = -100;
 
-    #region Case 3 - SingleWheel
-    [Test]
-    public void NextTransformedValues_SingleWheelLeftForeward_CorrectLeftTurnForewards()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = 0;
-        double right = 100;
+//     //     (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//     //     Assert.That(result.Item1, Is.EqualTo(0));
+//     //     Assert.That(result.Item2, Is.AtLeast(206));
+//     //     Assert.That(result.Item2, Is.AtMost(207));
+//     // }
+//     #endregion
 
-        Assert.That(result.Item1, Is.AtLeast(49.5));
-        Assert.That(result.Item1, Is.AtMost(50.5));
-        Assert.That(result.Item2, Is.AtLeast(-104));
-        Assert.That(result.Item2, Is.AtMost(-103));
-    }
+//     // #region Case 3 - SingleWheel
+//     // [Test]
+//     // public void NextTransformedValues_SingleWheelLeftForeward_CorrectLeftTurnForewards()
+//     // {
+//     //     short rawleft = 0;
+//     //     short rawright = 0;
+//     //     double left = 0;
+//     //     double right = 100;
 
-    [Test]
-    public void NextTransformedValues_SingleWheelRightForeward_CorrectRightTurnForewards()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = 100;
-        double right = 0;
+//     //     (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//     //     Assert.That(result.Item1, Is.AtLeast(49.5));
+//     //     Assert.That(result.Item1, Is.AtMost(50.5));
+//     //     Assert.That(result.Item2, Is.AtLeast(-104));
+//     //     Assert.That(result.Item2, Is.AtMost(-103));
+//     // }
 
-        Assert.That(result.Item1, Is.AtLeast(49.5));
-        Assert.That(result.Item1, Is.AtMost(50.5));
-        Assert.That(result.Item2, Is.AtLeast(103));
-        Assert.That(result.Item2, Is.AtMost(104));
-    }
+//     // [Test]
+//     // public void NextTransformedValues_SingleWheelRightForeward_CorrectRightTurnForewards()
+//     // {
+//     //     short rawleft = 0;
+//     //     short rawright = 0;
+//     //     double left = 100;
+//     //     double right = 0;
 
-    [Test]
-    public void NextTransformedValues_SingleWheelLeftBackward_CorrectLeftTurnBackwards()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = 0;
-        double right = -100;
+//     //     (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//     //     Assert.That(result.Item1, Is.AtLeast(49.5));
+//     //     Assert.That(result.Item1, Is.AtMost(50.5));
+//     //     Assert.That(result.Item2, Is.AtLeast(103));
+//     //     Assert.That(result.Item2, Is.AtMost(104));
+//     // }
 
-        Assert.That(result.Item1, Is.AtMost(-49.5));
-        Assert.That(result.Item1, Is.AtLeast(-50.5));
-        Assert.That(result.Item2, Is.AtLeast(103));
-        Assert.That(result.Item2, Is.AtMost(104));
-    }
+//     // [Test]
+//     // public void NextTransformedValues_SingleWheelLeftBackward_CorrectLeftTurnBackwards()
+//     // {
+//     //     short rawleft = 0;
+//     //     short rawright = 0;
+//     //     double left = 0;
+//     //     double right = -100;
 
-    [Test]
-    public void NextTransformedValues_SingleWheelRightBackward_CorrectRightTurnBackwards()
-    {
-        short rawleft = 0;
-        short rawright = 0;
-        double left = -100;
-        double right = 0;
+//     //     (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
 
-        (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+//     //     Assert.That(result.Item1, Is.AtMost(-49.5));
+//     //     Assert.That(result.Item1, Is.AtLeast(-50.5));
+//     //     Assert.That(result.Item2, Is.AtLeast(103));
+//     //     Assert.That(result.Item2, Is.AtMost(104));
+//     // }
 
-        Assert.That(result.Item1, Is.AtMost(-49.5));
-        Assert.That(result.Item1, Is.AtLeast(-50.5));
-        Assert.That(result.Item2, Is.AtLeast(-104));
-        Assert.That(result.Item2, Is.AtMost(-103));
-    }
-    #endregion
-}
+//     // [Test]
+//     // public void NextTransformedValues_SingleWheelRightBackward_CorrectRightTurnBackwards()
+//     // {
+//     //     short rawleft = 0;
+//     //     short rawright = 0;
+//     //     double left = -100;
+//     //     double right = 0;
+
+//     //     (double, double) result = Transformation!.TransformedValues_Next(rawleft, rawright, left, right);
+
+//     //     Assert.That(result.Item1, Is.AtMost(-49.5));
+//     //     Assert.That(result.Item1, Is.AtLeast(-50.5));
+//     //     Assert.That(result.Item2, Is.AtLeast(-104));
+//     //     Assert.That(result.Item2, Is.AtMost(-103));
+//     // }
+//     // #endregion
+// }
